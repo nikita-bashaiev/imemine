21d9369fd134a5b3bb1a126fd701b48d5f826639
Nikita Bashaiev <nikitabashaiev@gmail.com>
2024-01-26 14:19:25 +0100

    added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import '@styles/globals.css';
 import Header from '@components/Header';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Nikita Bashaiev',
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
